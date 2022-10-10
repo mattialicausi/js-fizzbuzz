c3056fabebc4010ae8c9aa2f7c8809c984f09255
mattialicausi <licausivitomattia@gmail.com>
2022-10-10 14:28:57 +0200

    aggiunto codice for per stampare in console le stringhe al posto dei numeri se sono multipli di 3 e 5, di 3, di 5.

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@
 // Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 // BONUS 1: Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 // BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+
+let riga = document.querySelector('row');
+
+
+for(let i = 1; i <= 100; i++){
+
+   if (((i % 3) == 0) && ((i % 5) == 0)){
+    console.log('fizzbuzz');
+   } else if((i % 5) == 0){
+    console.log('buzz');
+   } else if((i % 3) == 0){
+    console.log('fizz');
+   } else{
+    console.log(i);
+   }
+  
+}
