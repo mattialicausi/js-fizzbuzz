35043b97aa6ccd6f3b817b2d8f37dccdebea9538
mattialicausi <licausivitomattia@gmail.com>
2022-10-10 14:42:37 +0200

    aggiunto createElement per mostrare in html i box numerati

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 'use strict';
 
 // Scrivi un programma che stampi in console i numeri da 1 a 100, 
-// ma che per i multipli di 3 stampi “Fizz” al posto del numero 
-// e per i multipli di 5 stampi “Buzz”. 
-// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare?
+// x ma che per i multipli di 3 stampi “Fizz” al posto del numero 
+// x e per i multipli di 5 stampi “Buzz”. 
+// x Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
 // Consigli del giorno:
-// Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 // Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 // BONUS 1: Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 // BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 
-let riga = document.querySelector('row');
+let riga = document.querySelector('div.row');
 
+// CREO CICLO FOR PER STAMPARE IN CONSOLE I MULTIPLI DI 3 E 5 - 3 - 5
 
 for(let i = 1; i <= 100; i++){
 
@@ -26,5 +25,12 @@ for(let i = 1; i <= 100; i++){
    } else{
     console.log(i);
    }
-  
+
+
+   const myBox = document.createElement("div");
+   myBox.append('Box' + i);
+   riga.append(myBox);
+
 }
+
+
